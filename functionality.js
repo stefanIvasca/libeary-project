072a92aedfb9e88f160c4f6b8b9bb7c85bfcf36f
stefanIvasca <stefanivasca@gmail.com>
2022-12-19 15:54:28 +0200

    design books cards

--- functionality.js
+++ functionality.js
@@ -9,7 +9,7 @@ function displayBooks(){
         let bookPages = document.createElement('p');
         let bookRead = document.createElement('p');
         bookTitle.innerText = `${book.title}`;
-        bookAuthor.innerText = `${book.author}`;
+        bookAuthor.innerText = `by ${book.author}`;
         bookPages.innerText = `${book.pages} pages`;
         bookRead.innerText = `${book.read}`;
         bookContainer.appendChild(bookTitle);
@@ -38,5 +38,14 @@ Book.prototype.addBookToLibrary = function(){
 
 let newBook = new Book ('boos', 'me', 443, 'not read');
 newBook.addBookToLibrary();
+
+let secBook = new Book ('bodsfsos', 'meaa', 44, 'read');
+secBook.addBookToLibrary();
+
+let thrdBook = new Book ('What a book', 'Elon Gates', 52, 'not read');
+thrdBook.addBookToLibrary();
+
+let frthBook = new Book ('Not random', 'My Precious', 666, 'read');
+frthBook.addBookToLibrary();
 
 displayBooks();
