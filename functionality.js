078804cb3cc67b7e8019c5f144d7ab313480f3ec
stefanIvasca <stefanivasca@gmail.com>
2022-12-19 10:20:47 +0200

    add 2 default books in library

--- functionality.js
+++ functionality.js
@@ -1,4 +1,17 @@
-let myLibrary = [];
+let myLibrary = [
+    {
+        Title: "Ipoteza fericirii",
+        Author: "Jonathan Haidt",
+        Pages: 379,
+        read: "read"
+    },
+    {
+        Title: "Sunt o baba comunista",
+        Author: "Dan NuStiuCum",
+        Pages: 253,
+        read: "read"
+    }
+];
 
 function Book() {
     //constructor
